d2d3f92f9712a6824dc46bbd602e3640141da33a
Minji Kim <minzikimi@Minjis-MacBook-Pro.local>
2025-01-27 12:16:59 +0100

    removed a component

--- src/routes/WelcomePage.js
+++ src/routes/WelcomePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
-import './Welcome.css';
+
 
 const Welcome = () => {
   const [name, setName] = useState('');
