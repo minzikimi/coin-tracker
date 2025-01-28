f824901c976e6ecc96c57939eff264c5cc2fbed1
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-01-28 11:19:56 +0100

    Correct typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
 import WelcomePage from './routes/WelcomePage';
 import MainPage from './routes/MainPage';
-import WatchList from './routes/WatchList';
+import Watchlist from './routes/Watchlist';
 import Detail from './routes/Detail'; 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<WelcomePage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/watchlist" element={<WatchList />} />
+          <Route path="/watchlist" element={<Watchlist />} />
         </Routes>
         <Footer />
       </div>
