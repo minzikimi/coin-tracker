8f54a852612ff12c0f07001e82ede87c12fc5d5e
minzikimi <minji.kim@hyperisland.se>
2025-01-30 18:39:46 +0100

    added more css and animation

--- src/routes/MainPage/MainPage.js
+++ src/routes/MainPage/MainPage.js
@@ -1,85 +1,80 @@
-
 import { Link } from "react-router-dom";
 import "./MainPage.css";
 import useFetchCryptoData from "../../hooks/useFetchCryptoData";
 import Modal from "../../components/Modal/Modal";
 import { useState } from "react";
+import ShinyText from './ShinyText';
+import pepe from "../../assets/pepe-pepe-logo.svg";
 
 function MainPage() {
-  // const [loading, setLoading] = useState(true);
-  // const [coins, setCoins] = useState([]);
+  const { cryptoData: coins, loading } = useFetchCryptoData("tickers");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  function openModal () {
+    setIsModalOpen(true);
+  } 
+  function closeModal () {
+    setIsModalOpen(false);
+  }
 
-  // const API_URL = "https://api.coinpaprika.com/v1/tickers";
-
-  // useEffect(() => {
-  //   fetch(API_URL)
-  //     .then(response => {
-  //       if (!response.ok) throw new Error("Failed to fetch data");
-  //       return response.json();
-  //     })
-  //     .then(data => {
-  //       setCoins(data.slice(0, 100));
-  //       setLoading(false);
-  //     })
-  //     .catch(err => {
-
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-    const { cryptoData: coins, loading } = useFetchCryptoData("tickers");
-    const [isModalOpen,setOpenModal]=useState(false);
 
   return (
     <div>
-    
       <div className="main-page-text">
+      
         <h1>Your Crypto, Your Control: Track, Analyze, Succeed</h1>
         <p>Navigate the Crypto Landscape with Precision and Confidence</p>
+        <img 
+          src={pepe} 
+          alt="pepe" 
+          className="pepe" 
+          style={{ width: '150px', height: 'auto' }} 
+        />
       </div>
 
       <div className="button-wrapper">
+      
         <button 
-        className="openModalBtn"
-        onClick={()=>setOpenModal(true)}>Convert your coin!</button>
-        {isModalOpen && <Modal onClose={() => setOpenModal(false)} />}
+          className="openModalBtn"
+          onClick={openModal}
+        >
+         <ShinyText text="Convert your seed today!" disabled={false} speed={3} className='custom-class' />
+        </button>
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
       </div>
       
-     
       {loading ? (
         <strong>Loading...</strong>
       ) : ( 
         <div className="table-container">
           <table className="coin-table">
             <thead>
-                <tr>
-                  <th>Ranking</th>
-                  <th>Name</th>
-                  <th>Price</th>
+              <tr>
+                <th>Ranking</th>
+                <th>Name</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coins.map((coin) => (
+                <tr key={coin.id}>
+                  <td>{coin.rank}</td>
+                  <td>
+                    <Link to={`/detail/${coin.id}`} style={{ textDecoration: "none", color: "inherit" }}>
+                      <img
+                        src={`https://static.coinpaprika.com/coin/${coin.id}/logo.png`}
+                        alt={`${coin.name} icon`}
+                        style={{ width: "20px", height: "20px", marginRight: "10px" }}
+                      />
+                      {coin.name} ({coin.symbol})
+                    </Link>
+                  </td>
+                  <td>${parseFloat(coin.quotes.USD.price).toFixed(2)}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {coins.map((coin) => (
-                  <tr key={coin.id}>
-                    <td>{coin.rank}</td>
-                    <td>
-                      <Link to={`/detail/${coin.id}`} style={{ textDecoration: "none", color: "inherit" }}>
-                        <img
-                          src={`https://static.coinpaprika.com/coin/${coin.id}/logo.png`}
-                          alt={`${coin.name} icon`}
-                          style={{ width: "20px", height: "20px", marginRight: "10px" }}
-                        />
-                        {coin.name} ({coin.symbol})
-                      </Link>
-                    </td>
-                    <td>${parseFloat(coin.quotes.USD.price).toFixed(2)}</td>
-                  </tr>
-                ))}
-              </tbody>
+              ))}
+            </tbody>
           </table>
-      </div>
-        
+        </div>
       )}
     </div>
   );
