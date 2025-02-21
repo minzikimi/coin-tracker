3e51b07160004bb1470feb03f18d25cf1a139387
r-pears <ryjen.pears@gmail.com>
2025-02-21 17:21:03 +0100

    feedback

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,8 @@ import { Link } from "react-router-dom";
 import facebookLogo from "../../assets/social-facebook-svgrepo-com.svg";
 import instagramLogo from "../../assets/instagram-svgrepo-com.svg";
 import xLogo from "../../assets/icons8-x-50.png";
-import youtubeLogo from "../../assets/social-youtube-svgrepo-com.svg"
-import linkedinLogo from "../../assets/social-linkedin-svgrepo-com.svg"
-
-
+import youtubeLogo from "../../assets/social-youtube-svgrepo-com.svg";
+import linkedinLogo from "../../assets/social-linkedin-svgrepo-com.svg";
 
 function Footer() {
   return (
@@ -14,53 +12,44 @@ function Footer() {
       <div className="footer-content">
         <div className="footer-links">
           <ul>
-            <li><Link to="/main">Home</Link></li>
-            <li><Link to="/cryptocurrencies">Cryptocurrencies</Link></li>
-            <li><Link to="/watchlist">Watchlist</Link></li>
-            <li><Link to="/aboutus">About us</Link></li>
+            <li>
+              <Link to="/main">Home</Link>
+            </li>
+            <li>
+              <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </li>
+            <li>
+              <Link to="/watchlist">Watchlist</Link>
+            </li>
+            <li>
+              <Link to="/aboutus">About us</Link>
+            </li>
           </ul>
         </div>
 
+        {/* should be a list as well */}
         <div className="social-media">
           <a href="" target="_blank" rel="noopener noreferrer">
-              <img 
-                  src={xLogo} 
-                  alt="xsocial" 
-                  className="social-icon" 
-              />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <img 
-                  src={instagramLogo} 
-                  alt="Instagram" 
-                  className="social-icon" 
-              />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <img 
-                  src={facebookLogo} 
-                  alt="Facebook" 
-                  className="social-icon" 
-              />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <img 
-                  src={youtubeLogo} 
-                  alt="Youtube" 
-                  className="social-icon" 
-              />
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              <img 
-                  src={linkedinLogo} 
-                  alt="Linkedin" 
-                  className="social-icon" 
-              />
-            </a>
+            <img src={xLogo} alt="xsocial" className="social-icon" />
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src={instagramLogo} alt="Instagram" className="social-icon" />
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src={facebookLogo} alt="Facebook" className="social-icon" />
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src={youtubeLogo} alt="Youtube" className="social-icon" />
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinLogo} alt="Linkedin" className="social-icon" />
+          </a>
         </div>
 
         <div className="footer-text">
-          <p>&copy; {new Date().getFullYear()} Hyper Crypto. All rights reserved.</p>
+          <p>
+            &copy; {new Date().getFullYear()} Hyper Crypto. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
